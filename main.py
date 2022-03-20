26e9fe44efcaadea925b8d3a10d6c597ccda5c3c
Siraj Mussa <100541167+kingsiraj@users.noreply.github.com>
2022-03-20 23:20:24 +0000

    spelling mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("hello welcome to my math quizz this maths quizz will help lean and enjoy maths quizz")
+print("hello welcome to my math quizz this maths quizz will help learn and enjoy maths quiz")
+print("i hope you like my quiz")
 name=input("what is your name? ")
+
+
 
 
 
