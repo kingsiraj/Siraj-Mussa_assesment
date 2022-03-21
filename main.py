99133c87065d35600c97538d7b48df5523b277a5
Siraj Mussa <100541167+kingsiraj@users.noreply.github.com>
2022-03-21 21:19:04 +0000

    component 1 trial 2

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-print("hello welcome to my math quizz this maths quizz will help learn and enjoy maths quiz")
-print("i hope you like my quiz")
+print("Hello welcome to my math quizz.")
+print("This maths quizz will help learn and enjoy math")
+print("I hope you like my quiz")
 name=input("what is your name? ")
+while True:
+    try:
+        n = input("Please enter an age: ")
+        n = int(n)
+        break
+    except ValueError:
+        print("Not a valid integer! Please try again ...") 
+print("hello {},I hope you have fun playing my maths game".format(name))
+
 
 
 
